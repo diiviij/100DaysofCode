b3dcf90ec913d823c51415d936838a620252cc29
Divij Jain <68986989+diiviij@users.noreply.github.com>
2022-11-15 18:37:27 +0530

    Day10

---
+++ Day10/linkedlist.java
@@ -0,0 +1,16 @@
+import java.util.*;  
+public class LinkedList1{  
+ public static void main(String args[]){  
+  
+  LinkedList<String> al=new LinkedList<String>();  
+  al.add("Apple");  
+  al.add("Ball");  
+  al.add("Cat");  
+  al.add("Dog");  
+  
+  Iterator<String> itr=al.iterator();  
+  while(itr.hasNext()){  
+   System.out.println(itr.next());  
+  }  
+ }  
+}  
