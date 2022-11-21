bc99eedd248a878493eb7b902dca22671c9374ec
Divij Jain <68986989+diiviij@users.noreply.github.com>
2022-11-21 20:31:49 +0530

    Day16

--- Day16/weather_observation9_query.sql
+++ Day16/weather_observation9_query.sql
@@ -4,3 +4,4 @@ Query the list of CITY names from STATION that do not start with vowels. Your re
 select distinct city
 from station
 where not (city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or city like 'U%');
+
