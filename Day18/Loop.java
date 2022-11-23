74d414ec3d36e24af6a3150f5ca7b9a82a137e40
Divij Jain <68986989+diiviij@users.noreply.github.com>
2022-11-23 22:58:13 +0530

    Add files via upload

---
+++ Day18/Loop.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+public class Loop{
+    public static void main(String args[]){
+//--------------------------------------------------------------------  
+        //For Loop
+//--------------------------------------------------------------------
+
+//        int n=5,i,j;
+//        for(i=0;i<=n;i++){
+//          System.out.println(+i);
+//        }
+//--------------------------------------------------------------------
+      //Star Loop
+//--------------------------------------------------------------------
+
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // for(int line=1;line<=n;line++){
+        //     for(int j=1;j<=line;j++){
+        //         System.out.print("*");  
+        //     }    
+        //     System.out.println("");      
+        // }
+        
+//--------------------------------------------------------------------
+        //Reverse Star Loop
+//--------------------------------------------------------------------
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // for(int line=0;line<n;line++){
+        //     for(int j=n;j>line;j--){
+        //         System.out.print("*");  
+        //     }    
+        //     System.out.println("");      
+        // }
+// -----------------------------------------------------------------       
+// Half-Pyramid pattern number 
+//--------------------------------------------------------------------
+
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+// for(int j=1;j<=n;j++){
+//     for(int k=1;k<=j;k++){
+//         System.out.print(k);
+//     }
+//     System.out.println("");
+// }
+
+
+// -----------------------------------------------------------------       
+// Half-Pyramid pattern character pattern
+//--------------------------------------------------------------------
+
+
+// Scanner sc = new Scanner(System.in);
+// int n=9;
+// char ch = 'A';
+//  for(int j=1;j<=n;j++){
+//     for(int k=1;k<=j;k++){
+//         System.out.print(ch);
+//         ch++;
+//     }
+//    System.out.println("");
+// }
+
+    }
+}
