accd4f604c9fe899d07c905bc5b3439221e9c457
Divij Jain <68986989+diiviij@users.noreply.github.com>
2023-01-28 17:21:29 +0530

    Update cd.java

--- Day58/cd.java
+++ Day58/cd.java
@@ -1,1 +1,24 @@
 
+package Test7Hunts;
+import java.util.*;
+class rangeintsum
+{
+    public static void main(String args[])
+    {
+        int i, sum=0,start,end,k;
+        Scanner s = new Scanner(System.in);
+        start=s.nextInt();
+        end=s.nextInt();
+        k=s.nextInt();
+        s.close();
+       for(i=start;i<=end;i++)
+       {
+         if(i%k==0)
+         {
+            System.out.print(i+" ");
+            sum+=i;
+         }
+       }
+    System.out.println("\nThe sum : "+sum);
+    }
+}
