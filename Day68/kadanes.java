dcee5dd6044aac63e9028bd43a09bee176b27468
Divij Jain <68986989+diiviij@users.noreply.github.com>
2023-02-11 23:52:27 +0530

    Create kadanes.java

---
+++ Day68/kadanes.java
@@ -0,0 +1,14 @@
+public int kandaneForMaxSubArray(int[] arr) {
+int maxEndHere = 0;
+int maxSoFar = 0;
+for (int i = 0; i < arr.length; i++) {
+maxEndHere += arr[i];
+if (maxEndHere < 0) {
+maxEndHere = 0;
+}
+if (maxSoFar < maxEndHere) {
+maxSoFar = maxEndHere;
+}
+}
+return maxSoFar;
+}
